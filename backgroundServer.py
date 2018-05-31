0ca3272166cae1e469d6eb9c74d0fb7b4a5d9dcb
Srikar Prasad <srikasip@gmail.com>
2018-05-30 23:08:31 -0400

    dumb error that i should have tested both last time and this time

--- backgroundServer.py
+++ backgroundServer.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 @app.before_request
 def before_request():
   #if request.url.startswith('http://'):
-  if not request.is_secure():
+  if not request.is_secure:
     url = request.url.replace('http://', 'https://', 1)
     code = 301
     return redirect(url, code=code)
